e2efa62f4a0642102d4ba368fd1a6851d6691310
Javier Acosta <javieracost2@gmail.com>
2015-03-22 20:13:02 -0430

    meaningless commit

---
+++ Proyecto.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+"""
+TODO:
+	- leer/escribir archivos pbm
+	- procesar los pbm para que sirvan de entrada a la RN
+	- crear red neuronal
+	- disenhar interfaz
+	- implementar funcionalidades de la interfaz
+	- convertir entrada manual en pbm
+"""
